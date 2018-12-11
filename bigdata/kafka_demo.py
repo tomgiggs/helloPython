6bd0e751de775fa4ac581bc2ed29bfdc032ae38d
DESKTOP-Q5QVR0H\giggs <zamjbn@163.com>
2018-12-11 23:35:21 +0800

    some code

--- bigdata/kafka_demo.py
+++ bigdata/kafka_demo.py
@@ -1,4 +1,16 @@
 #encoding=utf8
 import kafka
+from kafka import KafkaProducer,KafkaConsumer
 
-consumer = kafka.KafkaConsumer
+producer = KafkaProducer(bootstrap_servers="localhost:9092", linger_ms=500, acks=1)#127.0.0.1和localhost不一样？，使用localhost就能连接上，127.0.0.1连接不上。。。。
+topic_name = 'flink_demo'
+message = "this is a flinke demo,will it be ok?"
+for i in range(200):
+    producer.send(topic=topic_name, value=message)
+producer.flush()
+producer.close()
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092',group_id='test001')
+consumer.subscribe("flink_demo")
+for x in consumer:
+    print(x)
+
